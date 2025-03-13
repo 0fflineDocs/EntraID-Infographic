37858f319a96f22b08d6b25dea6c8f5a43507de0
Simon H. <simon.ludvig.hakansson@outlook.com>
2025-03-13 15:41:32 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/entra-infographic/',
+  base: '/entraid-infographic/',
   plugins: [react()],
 });

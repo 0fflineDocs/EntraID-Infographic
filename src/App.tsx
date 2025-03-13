1a331e9f95a1d1d1547b52bab749dbd5fcd392b1
Simon H. <simon.ludvig.hakansson@outlook.com>
2025-03-13 15:33:58 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import EntraIDInfographic from './components/EntraIDInfographic';
+
+const App: React.FC = () => {
+  return <EntraIDInfographic />;
+};
+
+export default App;
